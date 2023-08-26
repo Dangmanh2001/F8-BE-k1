7338622d0c33ea92c66cd3e845c0f4fcfe7cbbaf
Dangmanh2001 <phaodai2000@gmail.com>
2023-08-27 04:59:41 +0700

    Nop bai tap buoi 19

---
+++ Baitapbuoi19/Baitapbuoi19.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE database_02_tenhocvien;
+
+USE database_02_tenhocvien;
+
+CREATE TABLE products(
+    id int NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    product_code varchar(50) NOT NULL,
+    price float NOT NULL,
+    quantity int NOT NULL,
+    total_number_of_product int,
+    total_amount_money float,
+    CONSTRAINT products_product_code_unique UNIQUE(product_code)
+);
+
+CREATE TABLE customers(
+    id int NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    phone varchar(13),
+    created_at timestamp,
+    updated_at timestamp,
+    CONSTRAINT customers_email_phone_unique UNIQUE(email,phone)
+);
+
+CREATE TABLE oder_list(
+    id int NOT NULL PRIMARY KEY,
+    customer_id int,
+    total_products int,
+    total_amount_money float,
+    oder_created_time timestamp,
+    oder_updated_time timestamp
+    FOREIGN KEY(customer_id) REFERENCES customer(id)
+);
+
+CREATE TABLE detail_order(
+    id int NOT NULL PRIMARY KEY,
+    customer_id int,
+    product_id
+)
+
