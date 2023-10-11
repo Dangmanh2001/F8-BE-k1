89c73251ca4018e5b243c4b9bbd2bd2ee11cd0fb
Dangmanh2001 <phaodai2000@gmail.com>
2023-10-11 14:30:02 +0700

    Update

--- Baitapbuoi31/app.js
+++ Baitapbuoi31/app.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const https = require("https");
+const expressSanitizer = require("express-sanitizer");
+const fs = require("fs");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -14,6 +17,11 @@ var loginRouter = require("./routes/login");
 const facebookPassport = require("./passport/facebookPassport");
 const passport = require("passport");
 const githubPassport = require("./passport/githubPassport");
+
+const options = {
+  key: fs.readFileSync("./config/cert.key"),
+  cert: fs.readFileSync("./config/cert.crt"),
+};
 
 var app = express();
 
@@ -34,15 +42,17 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(expressSanitizer());
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use(passport.initialize());
 app.use(passport.session());
+app.use("/", express.static("public"), indexRouter);
 
 passport.serializeUser(function (user, done) {
   process.nextTick(function () {
@@ -75,6 +85,10 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
+});
+
+https.createServer(options, app).listen(8080, () => {
+  console.log(`HTTPS server started on port 8080`);
 });
 
 module.exports = app;
